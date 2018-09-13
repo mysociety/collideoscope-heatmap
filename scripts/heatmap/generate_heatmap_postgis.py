835b03347123d9954646cd3c1bd6a05739e6fcd1
Dave Arter <davea@mysociety.org>
2018-09-13 15:39:41 +0100

    Do buffering within PostGIS; output heatmap shapefile

--- scripts/heatmap/generate_heatmap_postgis.py
+++ scripts/heatmap/generate_heatmap_postgis.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import fiona
 import records
-from shapely.geometry import box, CAP_STYLE
+from shapely.geometry import mapping, box, CAP_STYLE
 from shapely.wkb import loads
 from shapely.wkt import dumps
 
 DB = "postgres:///heatmap"
+OUTPUT_PATH = "output/heatmap.shp"
+
 BUFFER = 25
 
 def main():
@@ -13,24 +16,40 @@ def main():
     print(db.query("SELECT COUNT(*) FROM roads")[0].count)
     print(db.query("SELECT COUNT(*) FROM incidents")[0].count)
 
-    matches = 0
-    for road in db.query("SELECT * FROM roads"):
-        geom = loads(road.geom, hex=True)
-        bbox = box(*geom.bounds).buffer(BUFFER, cap_style=CAP_STYLE.square)
-        incidents = db.query("SELECT geom FROM incidents WHERE geom && ST_GeomFromText(:wkt, 27700)", wkt=dumps(bbox))
+    meta = {
+            'crs': {'init': 'epsg:27700'},
+            'driver': 'ESRI Shapefile',
+            'schema': {
+                'geometry': 'LineString',
+                'properties': {
+                    'density': 'float'
+                }
+            }
+        }
 
-        buffered = None
-        count = 0
-        for incident in incidents:
-            if buffered is None:
-                buffered = geom.buffer(BUFFER, cap_style=CAP_STYLE.flat)
-            i_geom = loads(incident.geom, hex=True)
-            if buffered.contains(i_geom):
-                count += 1
-        if count:
-            # print(geom.length, count)
-            matches +=1
-    print(matches)
+    with fiona.open(OUTPUT_PATH, 'w', **meta) as output:
+        matches = 0
+        for road in db.query("SELECT * FROM roads"):
+            geom = loads(road.geom, hex=True)
+            q = """SELECT
+                    COUNT(*)
+                   FROM incidents
+                   WHERE
+                    geom && ST_Envelope(ST_Buffer(ST_GeomFromText(:wkt, 27700), :distance))
+                    AND ST_Contains(ST_Buffer(ST_GeomFromText(:wkt, 27700), :distance), geom)
+                   """
+            count = db.query(q, wkt=dumps(geom), distance=BUFFER)[0].count
+            if count:
+                matches +=1
+                output.write({
+                    'type': 'Feature',
+                    'id': '-1',
+                    'geometry': mapping(geom),
+                    'properties': {
+                        'density': count / geom.length
+                    }
+                })
+        print(matches)
 
 
 
