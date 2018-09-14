5e4ef109c50fd4ac639e3f6cda46010db5855e5b
Dave Arter <davea@mysociety.org>
2018-09-14 09:15:01 +0100

    Import roads from multiple shapefiles

--- scripts/heatmap/load_into_postgis.sh
+++ scripts/heatmap/load_into_postgis.sh
@@ -5,12 +5,20 @@ set -x
 
 DBNAME="heatmap"
 COLLIDEOSCOPE_DBNAME="collideoscope"
-ROADS_SHP="/Users/davea/Code/fixmystreet/collideoscope/scripts/heatmap/data/roads.shp"
+ROADS_SHPS="/Users/davea/Code/fixmystreet/collideoscope/scripts/heatmap/data/roads"
 
 dropdb $DBNAME
 createdb -T template_postgis $DBNAME
 
-shp2pgsql -I -S -s EPSG:27700 -D -d "$ROADS_SHP" roads | psql -q -d $DBNAME
+for SHP in $ROADS_SHPS/*.shp; do
+    shp2pgsql -S -s EPSG:27700 -D -p "$SHP" roads | psql -q -d $DBNAME
+    break # Just create the table from the first file
+done
+
+for SHP in $ROADS_SHPS/*.shp; do
+    shp2pgsql -S -s EPSG:27700 -D -a "$SHP" roads | psql -q -d $DBNAME
+done
+
 psql -d $DBNAME -c "SELECT COUNT(*) FROM roads"
 
 ogr2ogr -f PostgreSQL -t_srs EPSG:27700 -lco GEOMETRY_NAME=geom -nln incidents PG:"dbname=$DBNAME" PG:"dbname=$COLLIDEOSCOPE_DBNAME" -sql "SELECT ST_SetSRID(ST_MakePoint(longitude, latitude), 4326) as geom, id FROM problem"
