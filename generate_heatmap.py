14965214ee1067a0104c3d418dbacc44f748e59c
Dave Arter <davea@mysociety.org>
2018-09-17 18:05:50 +0100

    Read from OS Open Roads zip directly instead of need to have it extracted.
    Reads config from environment variables.

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -1,45 +1,77 @@
 #!/usr/bin/env python3
+import os
 from datetime import datetime
 from functools import partial
+from zipfile import ZipFile
+from tempfile import TemporaryDirectory
 
 import records
-from shapely.geometry import mapping, box, CAP_STYLE
-from shapely.wkb import loads
+from shapely.geometry import shape, mapping, box, CAP_STYLE
 from shapely.wkt import dumps
 from shapely.ops import transform
 import fiona
 import pyproj
 
-DB = "postgres:///heatmap"
-OUTPUT_PATH = "output/heatmap.gpkg"
 
-BUFFER = 25
+# These environment variables determine the data sources used by the script.
+# The first two are mandatory.
 
-reproject = partial(
-    pyproj.transform,
-    pyproj.Proj(init='epsg:27700'),
-    pyproj.Proj(init='epsg:3857'))
+# This should be the path to the OS Open Roads zip file (oproad_essh_gb.zip).
+# It does not need to be unzipped - the script handles that.
+OS_OPEN_ROADS_PATH = os.environ['OS_OPEN_ROADS_PATH']
+
+# This is the PostGIS database where load_into_postgis.sh stored the incidents.
+DATABASE_URL = os.environ['DATABASE_URL']
+
+# Optional, sets where the output GeoPackage will be written. By default
+# it will be 'heatmap.gpkg' in the working directory.
+OUTPUT_PATH = os.environ.get('OUTPUT_PATH', 'heatmap.gpkg')
+
+# Optional, controls the distance (in metres) to buffer each road feature by
+# when determining incident density.
+BUFFER = int(os.environ.get("BUFFER", 25))
+
+
+def read_shapefile(path):
+    print("Processing {}".format(os.path.basename(path)))
+    with fiona.open(path, 'r') as shapefile:
+        for feature in shapefile:
+            yield shape(feature['geometry'])
+
+
+def load_roads():
+    with ZipFile(OS_OPEN_ROADS_PATH) as z:
+        for shp in (f for f in z.namelist() if f.endswith("RoadLink.shp")):
+            prefix = shp.rsplit(".", 1)[0]
+            files = ["{}.{}".format(prefix, ext) for ext in ["dbf", "prj", "shp", "shx"]]
+            with TemporaryDirectory() as tmpdir:
+                for file in files:
+                    z.extract(file, path=tmpdir)
+                yield from read_shapefile(os.path.join(tmpdir, shp))
+
 
 def main():
-    db = records.Database(DB)
-    print(db.query("SELECT COUNT(*) FROM roads")[0].count)
-    print(db.query("SELECT COUNT(*) FROM incidents")[0].count)
+    db = records.Database(DATABASE_URL)
+
+    reproject = partial(
+        pyproj.transform,
+        pyproj.Proj(init='epsg:27700'),
+        pyproj.Proj(init='epsg:3857'))
 
     meta = {
-            'crs': {'init': 'epsg:3857'},
-            'driver': 'GPKG',
-            'schema': {
-                'geometry': 'LineString',
-                'properties': {
-                    'density': 'float'
-                }
+        'crs': {'init': 'epsg:3857'},
+        'driver': 'GPKG',
+        'schema': {
+            'geometry': 'LineString',
+            'properties': {
+                'density': 'float'
             }
         }
+    }
 
     with fiona.open(OUTPUT_PATH, 'w', **meta) as output:
         matches = 0
-        for i, road in enumerate(db.query("SELECT * FROM roads"), start=1):
-            geom = loads(road.geom, hex=True)
+        for i, geom in enumerate(load_roads(), start=1):
             buffered = geom.buffer(BUFFER, cap_style=CAP_STYLE.flat)
             bbox = box(*buffered.bounds)
             q = """SELECT
@@ -60,10 +92,9 @@ def main():
                         'density': count / geom.length
                     }
                 })
-                if matches % 1000 == 0:
+                if matches % 1000 == 0 or i % 1000 == 0:
                     print(datetime.now(), i, matches)
         print(datetime.now(), i, matches)
-
 
 
 if __name__ == '__main__':
--- load_into_postgis.sh
+++ load_into_postgis.sh
@@ -5,21 +5,9 @@ set -x
 
 DBNAME="heatmap"
 COLLIDEOSCOPE_DBNAME="collideoscope"
-ROADS_SHPS="/Users/davea/Code/fixmystreet/collideoscope/scripts/heatmap/data/roads"
 
 dropdb $DBNAME
 createdb -T template_postgis $DBNAME
-
-for SHP in $ROADS_SHPS/*.shp; do
-    shp2pgsql -S -s EPSG:27700 -N skip -t 2D -D -p "$SHP" roads | psql -q -d $DBNAME
-    break # Just create the table from the first file
-done
-
-for SHP in $ROADS_SHPS/*.shp; do
-    shp2pgsql -S -s EPSG:27700 -N skip -t 2D -D -a "$SHP" roads | psql -q -d $DBNAME
-done
-
-psql -d $DBNAME -c "SELECT COUNT(*) FROM roads"
 
 ogr2ogr -f PostgreSQL -t_srs EPSG:27700 -lco GEOMETRY_NAME=geom -nln incidents PG:"dbname=$DBNAME" PG:"dbname=$COLLIDEOSCOPE_DBNAME" -sql "SELECT ST_SetSRID(ST_MakePoint(longitude, latitude), 4326) as geom, id FROM problem"
 psql -d $DBNAME -c "SELECT COUNT(*) FROM incidents"
